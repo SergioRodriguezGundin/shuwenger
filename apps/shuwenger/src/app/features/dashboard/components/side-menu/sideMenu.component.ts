146bceed01ed1505ff5018e39f3bc91142c4bc10
Sergio-RG <40829363+SergioRodriguezGundin@users.noreply.github.com>
2025-02-22 11:08:33 +0100

    Updated side menu

--- apps/shuwenger/src/app/features/dashboard/components/side-menu/sideMenu.component.ts
+++ apps/shuwenger/src/app/features/dashboard/components/side-menu/sideMenu.component.ts
@@ -1,28 +1,35 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import {
+  ActivatedRoute,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import {
   lucideCalendar,
   lucideCar,
   lucideHouse,
+  lucideSettings,
   lucideUser,
 } from '@ng-icons/lucide';
-import { BrnCommandImports } from '@spartan-ng/brain/command';
-import { HlmCommandImports } from '@spartan-ng/ui-command-helm';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-side-menu',
   templateUrl: './sideMenu.component.html',
-  imports: [HlmCommandImports, BrnCommandImports, NgIcon],
+  imports: [NgIcon, RouterLink, RouterLinkActive],
   providers: [
-    provideIcons({ lucideCalendar, lucideHouse, lucideCar, lucideUser }),
+    provideIcons({
+      lucideCalendar,
+      lucideHouse,
+      lucideCar,
+      lucideUser,
+      lucideSettings,
+    }),
   ],
 })
 export class SideMenuComponent {
   protected readonly router = inject(Router);
   protected readonly route = inject(ActivatedRoute);
-  navigateTo(route: string) {
-    this.router.navigate([route], { relativeTo: this.route });
-  }
 }
