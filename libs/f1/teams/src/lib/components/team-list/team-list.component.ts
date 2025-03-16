92f0b977198321b73e5e67b795324402a871ff2f
Sergio-RG <40829363+SergioRodriguezGundin@users.noreply.github.com>
2025-03-16 13:13:28 +0100

    Set team standings styles

--- libs/f1/teams/src/lib/components/team-list/team-list.component.ts
+++ libs/f1/teams/src/lib/components/team-list/team-list.component.ts
@@ -1,3 +1,4 @@
+import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TeamsStore } from '../../store/teams.store';
@@ -5,7 +6,7 @@ import { TeamsStore } from '../../store/teams.store';
 @Component({
   selector: 'lib-f1-team-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './team-list.component.html',
   styleUrls: ['./team-list.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
